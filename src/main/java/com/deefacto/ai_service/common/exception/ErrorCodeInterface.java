b857553bfbe8725a2c0bc1f456a1b049c531165e
Jeung9711 <134358261+Jeung9711@users.noreply.github.com>
2025-08-11 02:45:56 +0900

    feat|sprint2|해당없음|ì기본 설정 파ì 및 환경 변수 추가

---
+++ src/main/java/com/deefacto/ai_service/common/exception/ErrorCodeInterface.java
@@ -0,0 +1,9 @@
+package com.deefacto.ai_service.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCodeInterface {
+    String getCode();
+    String getMessage();
+    HttpStatus getStatus();
+}
