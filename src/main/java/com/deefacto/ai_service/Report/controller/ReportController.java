d1a0360d3739a1bc3275ea92d8922cdbf980faa8
Jeung9711 <134358261+Jeung9711@users.noreply.github.com>
2025-08-12 11:06:59 +0900

    feat|sprint2|LC-460|권한 확인 후 ë리포트 다운로드 |Mo

--- src/main/java/com/deefacto/ai_service/Report/controller/ReportController.java
+++ src/main/java/com/deefacto/ai_service/Report/controller/ReportController.java
@@ -2,9 +2,11 @@ package com.deefacto.ai_service.Report.controller;
 
 import com.deefacto.ai_service.Report.Service.S3Service;
 import com.deefacto.ai_service.common.dto.ApiResponseDto;
+import com.deefacto.ai_service.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,26 +18,30 @@ import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/report")
+@RequestMapping("/reports")
 public class ReportController {
     private final S3Service s3Service;
 
 
 
     @GetMapping("/download/{fileName}")
-    public ResponseEntity<InputStreamResource> downloadPdf(
-            @PathVariable String fileName
+    public ResponseEntity<?> downloadPdf(
+            @PathVariable String fileName,
+            @RequestHeader("X-Role") String role
     ) throws IOException {
-        System.out.println("start controller");
-        System.out.println("AWS_ACCESS_KEY_ID: " + System.getenv("AWS_ACCESS_KEY_ID"));
-        System.out.println("AWS_REGION: " + System.getenv("AWS_REGION"));
-        InputStream fileStream = s3Service.downloadFile(fileName);
 
-        String encodedFileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8)
-                .replace("\\+","%20");
+        String roleKey = role.substring(role.lastIndexOf('_') + 1);
 
-        return  ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+encodedFileName+"\"")
+        if (!fileName.contains(roleKey)) {
+            ApiResponseDto<String> errorBody = ApiResponseDto.createError(ErrorCode.FORBIDDEN.getCode(), ErrorCode.FORBIDDEN.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorBody);
+        }
+
+        // 검증 통과 시 다운로드 처리
+        InputStream fileStream = reportService.downloadFile(fileName);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
                 .contentType(MediaType.APPLICATION_PDF)
                 .body(new InputStreamResource(fileStream));
     }
